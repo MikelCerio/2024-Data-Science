b88ce384610880e5640e100f820476010d525586
MikelCerio <mikelcerio@gmail.com>
2024-05-13 18:10:10 +0200

    lo que sea

---
+++ 1-Ramp_Up/Python/4-Funciones/Practica/funciones.py
@@ -0,0 +1,12 @@
+# funciones.py
+
+from math import pi
+
+def area_cuadrado(lado):
+    return lado * lado
+
+def area_triangulo(base,altura):
+    return (base * altura) / 2
+
+def area_circulo(radio):
+    return pi * radio **2
